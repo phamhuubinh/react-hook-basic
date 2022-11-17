ddacd9fb9b5a4603f81df56060a60ae85bd1eb73
phamhuubinh <phamgiabinh95@gmail.com>
2022-11-17 14:59:25 +0700

    17112022

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './view/Nav';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('Minh')
+  const [address, setAddress] = useState('');
+  const [todos, setTodos] = useState([
+    { id: 'todo1', title: 'Watching youtube' },
+    { id: 'todo2', title: 'Doing homework' },
+    { id: 'todo3', title: 'Playing game' }
+  ]);
+
+  const handleOnChangeInput = (event) => {
+    setAddress(event.target.value)
+  }
+  const handleEventClick = () => {
+    if (!address) {
+      alert('emtpy input')
+      return;
+    }
+    let newTodo = { id: '', title: address }
+    setTodos([...todos, newTodo]);
+    setAddress('');
+  }
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1>Xin chào {name}</h1>
+        <div className='todo-container'>
+          {todos.map(todo => {
+            return (
+              <li
+                className='todo-child'
+                style={{ textAlign: 'left' }}
+                key={todo.id}
+              >
+                {todo.title}
+              </li>
+            )
+          })}
+        </div>
+        <input
+          type='text'
+          value={address}
+          onChange={(event) => handleOnChangeInput(event)}
+        />
+        <button
+          type='button'
+          onClick={handleEventClick}>
+          Click me
+        </button>
+      </header >
+    </div >
   );
 }
 
---
+++ src/view/Nav.js
@@ -0,0 +1,14 @@
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
+            <a href="#new">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    );
+}
+
+export default Nav;
